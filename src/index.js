2d9a5c13a12adf02dbe207c68ac9f9a21d86f8f2
p1ch_jp <p1ch.jp@gmail.com>
2016-02-16 21:23:48 +0900

    Add hello phina.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const phina = require('phina.js');
+
+phina.globalize();
+
+// MainScene クラスを定義
+phina.define('MainScene', {
+  superClass: 'CanvasScene',
+  init: function() {
+    this.superInit();
+    // 背景色を指定
+    this.backgroundColor = '#444';
+    // ラベルを生成
+    this.label = Label('Hello, phina.js!').addChildTo(this);
+    this.label.x = this.gridX.center(); // x 座標
+    this.label.y = this.gridY.center(); // y 座標
+    this.label.fill = 'white'; // 塗りつぶし色
+  }
+});
+
+// メイン処理
+phina.main(function() {
+  // アプリケーション生成
+  const app = GameApp({
+    startLabel: 'main', // メインシーンから開始する
+  });
+  // アプリケーション実行
+  app.run();
+});
